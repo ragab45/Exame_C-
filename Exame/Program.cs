ec57b11b90c453728a94165cc2e19ffbc6f31a76
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-07-12 18:46:20 +0300

    Q-1

--- Exame/Program.cs
+++ Exame/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q-1
+            Console.WriteLine("Enter your age");
+            String outage = (Console.ReadLine());
+            int age;
+            if(!int.TryParse(outage, out age))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid age.");
+                return;
+            }
+            Console.WriteLine("Enter your Nationality");
+            String Nationality = Console.ReadLine();
+            if (age >= 18 && Nationality.Equals("Egyptian"))
+            {
+                Console.WriteLine("You are eligible to vote");
+            }
+            else
+            {
+                Console.WriteLine("You are not eligible to vote");
+            }
+            #endregion
         }
     }
 }
